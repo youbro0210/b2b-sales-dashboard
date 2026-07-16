63af9d0e392106faebfbf667c77031cb8f7faff5
youbro0210 <youbro0210@gmail.com>
2026-07-16 10:14:45 +0900

    Create page.tsx

---
+++ app/(app)/loading/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import LoadingBoard from "@/components/LoadingBoard";
+
+// 마트 = 오프라인 채널 + 수수료업체
+const GROUPS = ["오프라인", "롯데마트_수수료업체", "이마트_수수료업체"];
+
+export default function MartPage() {
+  return <LoadingBoard title="B2C 오프라인" groups={GROUPS} />;
+}
