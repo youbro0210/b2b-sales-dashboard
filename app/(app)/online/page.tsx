ceba60692168a3307e488c3d876edb16f2b429c7
youbro0210 <youbro0210@gmail.com>
2026-07-16 10:15:06 +0900

    Delete app/(app)/online/page.tsx

--- app/(app)/online/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import LoadingBoard from "@/components/LoadingBoard";
-
-const GROUPS = ["온라인"];
-
-export default function OnlinePage() {
-  return <LoadingBoard title="온라인" groups={GROUPS} />;
-}
