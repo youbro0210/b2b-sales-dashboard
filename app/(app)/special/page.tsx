710694a625cb57ba76df1164a99e7bd470faf7ad
youbro0210 <youbro0210@gmail.com>
2026-07-16 11:38:10 +0900

    Delete app/(app)/special/page.tsx

--- app/(app)/special/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import LoadingBoard from "@/components/LoadingBoard";
-
-const GROUPS = ["특정"];
-
-export default function SpecialPage() {
-  return <LoadingBoard title="특정" groups={GROUPS} />;
-}
